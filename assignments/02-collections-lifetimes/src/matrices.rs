97240a57d8eaf7e33432b3176c002681eb4d1f72
Brooke Wager <bwager@olin.edu>
2024-10-01 18:05:30 -0400

    Complete all parts of assignment-02

--- assignments/02-collections-lifetimes/src/matrices.rs
+++ assignments/02-collections-lifetimes/src/matrices.rs
@@ -5,19 +5,55 @@ pub enum MatrixError {
     InvalidShape,
 }
 
-fn dot_product_prescriptive(vec1: &Vec<f64>, vec2: &Vec<f64>) -> Result<f64, MatrixError> {
-    todo!()
+fn dot_product_prescriptive(vec1: &[f64], vec2: &[f64]) -> Result<f64, MatrixError> {
+    if vec1.is_empty() || vec2.is_empty() {
+        return Err(MatrixError::EmptyVector);
+    }
+    if vec1.len() != vec2.len() {
+        return Err(MatrixError::DimensionMismatch);
+    }
+    let mut result = 0.0;
+    for dim in 0..vec1.len() {
+        result += vec1[dim] * vec2[dim];
+    }
+    Ok(result)
 }
 
-fn dot_product_functional(vec1: &Vec<f64>, vec2: &Vec<f64>) -> Result<f64, MatrixError> {
-    todo!()
+fn dot_product_functional(vec1: &[f64], vec2: &[f64]) -> Result<f64, MatrixError> {
+    vec1.iter().zip(vec2.iter()).fold(
+        if vec1.is_empty() || vec2.is_empty() {
+            Err(MatrixError::EmptyVector)
+        } else if vec1.len() != vec2.len() {
+            Err(MatrixError::DimensionMismatch)
+        } else {
+            Ok(0.0)
+        },
+        |result, (a, b)| result.map(|result| result + a * b),
+    )
 }
 
-fn multiply_matrices(
-    vec1: &Vec<Vec<f64>>,
-    vec2: &Vec<Vec<f64>>,
-) -> Result<Vec<Vec<f64>>, MatrixError> {
-    todo!()
+fn multiply_matrices(vec1: &[Vec<f64>], vec2: &[Vec<f64>]) -> Result<Vec<Vec<f64>>, MatrixError> {
+    if vec1.is_empty() || vec2.is_empty() {
+        return Err(MatrixError::EmptyVector);
+    }
+    if vec1.iter().any(|row| row.len() != vec1[0].len())
+        || vec2.iter().any(|row| row.len() != vec2[0].len())
+    {
+        return Err(MatrixError::InvalidShape);
+    }
+    if vec1[0].len() != vec2.len() {
+        return Err(MatrixError::DimensionMismatch);
+    }
+
+    let mut result = vec![vec![0.0; vec2[0].len()]; vec1.len()];
+    for i in 0..vec1.len() {
+        for j in 0..vec2[0].len() {
+            for (k, item) in vec2.iter().enumerate().take(vec1[0].len()) {
+                result[i][j] += vec1[i][k] * item[j];
+            }
+        }
+    }
+    Ok(result)
 }
 
 #[cfg(test)]
